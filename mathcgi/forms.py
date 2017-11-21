8285fa9caa3a902c20362bd46dffb17008468f6c
Alex <alexrtrost@gmail.com>
2017-11-21 05:57:19 -0500

    Created CGIForm for students to answer on

--- mathcgi/forms.py
+++ mathcgi/forms.py
@@ -14,5 +14,7 @@ class CGIResultsForm(forms.ModelForm):
         }
 
 
-
-
+class CGIForm(forms.Form):
+    answer = forms.IntegerField()
+    # Need to send through the numbers that the student worked with, as they are random. have to lock them in so we can
+    # Check the answer and see if it's correct.
