a4da089ea115b87820af26b26fc75b1161e2e8ec
Alex <alexrtrost@gmail.com>
2017-11-25 23:14:32 -0500

    Added Delete view, made editing challenge exercises work and added some styles.

--- ixl/forms.py
+++ ixl/forms.py
@@ -14,13 +14,67 @@ from .models import IXLListAssignment, IXLList
 class CreateIXLListForm(forms.Form):
     title = forms.CharField()
     category = forms.ChoiceField(choices=(("Unit", "Unit"), ("Remediation", "Remediation"), ("Enrichment", "Enrichment"), ("Test", "Test"), ("Other", "Other")))
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': 3, 'cols': 15}))
 
 
-class CreateIXLListExerciseForm(forms.Form):
+class IXLListExerciseForm(forms.Form):
     ixl_format = RegexValidator(r'^\w+\.\d+$', 'Pattern must match IXL format: A.12')
     grade = forms.CharField(max_length=1, widget=forms.TextInput(attrs={'size': '2'}))
     id_code = forms.CharField(validators=[ixl_format], max_length=5)
-    required_score = forms.CharField(label="Required Score")
+    required_score = forms.CharField(label="Required Score", initial=80)
+
+
+from django.forms.formsets import BaseFormSet
+
+
+class BaseIXLListExerciseFormSet(BaseFormSet):
+    def clean(self):
+        """
+        Adds validation to check that the same exercise does not appear twice
+        """
+        if any(self.errors):
+            return
+        # This will hold Tuples with grade, id_code, required_score:
+        grade_id_scores = []
+        duplicates = False
+
+        for form in self.forms:
+            if form.cleaned_data:
+                grade = form.cleaned_data['grade']
+                id_code = form.cleaned_data['id_code']
+                required_score = form.cleaned_data['required_score']
+                if not required_score:
+                    required_score = 80
+                if not grade:
+                    raise forms.ValidationError("You need to enter a grade!")
+                if grade and id_code and required_score:
+                    if (grade, id_code, required_score) in grade_id_scores:
+                        duplicates = True
+                        grade_id_scores.append((grade, id_code, required_score))
+
+                if duplicates:
+                    raise forms.ValidationError(
+                        'Exercises must be unique. You can only have the same exercise in twice if they have different Required Scores.',
+                        code='duplicate_links'
+                    )
+
+                    # Check that all links have both an anchor and URL
+                    # if url and not anchor:
+                    #     raise forms.ValidationError(
+                    #         'All links must have an anchor.',
+                    #         code='missing_anchor'
+                    #     )
+                    # elif anchor and not url:
+                    #     raise forms.ValidationError(
+                    #         'All links must have a URL.',
+                    #         code='missing_URL'
+                    #     )
+
+
+
+
+
+
 
 class ListSettings(forms.Form):
     bonus_challenges = forms.IntegerField(initial=15)
--- ixl/views.py
+++ ixl/views.py
@@ -1,6 +1,6 @@
 import re
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
@@ -9,8 +9,9 @@ from django.views import generic
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.forms import formset_factory
+from django.db import transaction, IntegrityError
 
-from .forms import CreateIXLListForm, CreateIXLListExerciseForm
+from .forms import CreateIXLListForm, IXLListExerciseForm, BaseIXLListExerciseFormSet
 
 from ixl.models import IXLSkill, IXLSkillScores, IXLList, IXLListAssignment, IXLListExercise, IXLListSkill
 
@@ -60,6 +61,8 @@ def ixl_list_create(request):
             ixl_list.author = request.user
             ixl_list.title = form.cleaned_data['title']
             ixl_list.category = form.cleaned_data['category']
+            ixl_list.description = form.cleaned_data['description']
+
             ixl_list.save()
             return HttpResponseRedirect('/ixl/lists/view/')  # Soon this will send us to the assign exercises page
     else:
@@ -69,11 +72,58 @@ def ixl_list_create(request):
 
 
 def ixl_list_exercise_assign(request, list):
-    pass
+    """
+        Allows a user to add exercises to an IXL list.
+    """
 
+    ixl_list = get_object_or_404(IXLList, pk=list)
 
+    user = request.user
 
+    # Create the formset, specifying the form and formset we want to use.
+    IXLListExerciseFormSet = formset_factory(IXLListExerciseForm, formset=BaseIXLListExerciseFormSet)
+    # Get our existing  data for this list.  This is used as initial data.
+    ixl_list_exercises = IXLListExercise.objects.filter(list=ixl_list).order_by('order')
+    ixl_list_exercise_data = [
+        {'grade': l.list_skill.grade, 'id_code': l.list_skill.id_code, 'required_score': l.required_score,
+         'list': l.list, 'order': l.order} for l in ixl_list_exercises]
 
+    if request.method == "POST":
+        ixl_list_exercise_formset = IXLListExerciseFormSet(request.POST)
+        if ixl_list_exercise_formset.is_valid():
+            new_exercises = []
+            for order, exercise_form in enumerate(ixl_list_exercise_formset):
+                try:
+                    grade = exercise_form.cleaned_data['grade']
+                    id_code = exercise_form.cleaned_data['id_code']
+                    required_score = exercise_form.cleaned_data['required_score']
+                    if grade and id_code and required_score:
+                        ixl_list_skill = IXLListSkill.objects.filter(grade=grade, id_code=id_code).first()
+                        new_exercises.append(
+                            IXLListExercise(list_skill=ixl_list_skill, required_score=required_score, order=order,
+                                            list=ixl_list))
+                except:
+                    pass
+            try:
+                with transaction.atomic():
+                    # Replace the old with the new
+                    IXLListExercise.objects.filter(list=ixl_list).delete()
+                    IXLListExercise.objects.bulk_create(new_exercises)
+
+                    # And notify our users that it worked
+                    messages.success(request, 'You have updated the list: {}'.format(ixl_list.title))
+
+            except IntegrityError:  # If the transaction failed
+                messages.error(request, 'There was an error saving the list.')
+                return HttpResponseRedirect('/ixl/lists/view/')
+
+    ixl_list_exercise_formset = IXLListExerciseFormSet(initial=ixl_list_exercise_data)
+
+    context = {
+        'list': ixl_list,
+        'formset': ixl_list_exercise_formset,
+    }
+    return render(request, 'ixl/ixl_list_assign_exercises.html', context)
 
 
 
@@ -88,4 +138,38 @@ def view_lists(request):
 
 def view_single_list(request, list):
     list = get_object_or_404(IXLList, pk=list)
-    return render(request, 'ixl/view_single_list.html', {'list':list,})
+    list_exercises = IXLListExercise.objects.filter(list=list).order_by('order')
+    return render(request, 'ixl/view_single_list.html', {'list': list, 'list_exercises': list_exercises})
+
+
+def edit_list(request, list):
+    list = get_object_or_404(IXLList, pk=list)
+    if request.method == 'POST':
+        form = CreateIXLListForm(request.POST)
+        if form.is_valid():
+            ixl_list = list
+            ixl_list.author = request.user
+            ixl_list.title = form.cleaned_data['title']
+            ixl_list.category = form.cleaned_data['category']
+            ixl_list.description = form.cleaned_data['description']
+            ixl_list.save()
+            return HttpResponseRedirect('/ixl/lists/view/')  # Soon this will send us to the assign exercises page
+    else:
+        form = CreateIXLListForm(
+            initial={'title': list.title, 'category': list.category, 'description': list.description})
+    return render(request, 'ixl/ixl_list_create_form.html', {'form': form, })
+
+
+from django.http import Http404
+
+
+def delete_list(request, list):
+    user = request.user
+    list = get_object_or_404(IXLList, pk=list)
+    if not user == list.author:
+        raise Http404
+    if request.method == "POST":
+        messages.success(request, 'You have deleted the list: {}'.format(list.title))
+        list.delete()
+        return HttpResponseRedirect('/ixl/lists/view/')
+    return render(request, 'ixl/ixl_list_delete.html', {'list': list})
